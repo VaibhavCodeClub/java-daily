8398602bc9e84810ba411e168b0d9b213fee491d
sapatevaibhav <sapatevaibhav@duck.com>
2024-03-09 22:59:19 +0530

    Maximum subarray

---
+++ day1 array /leet53.java
@@ -0,0 +1,23 @@
+// Maximum subarray
+// Given an integer array nums, find the subarray with the largest sum, and return its sum.
+// Kadan's Algorithm
+
+public class leet53 {
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[] { -1 }));
+    }
+
+    public static int maxSubArray(int[] nums) {
+        int maxSum = nums[0], curSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curSum = curSum + nums[i];
+            if (curSum > maxSum) {
+                maxSum = curSum;
+            }
+            if (curSum < 0) {
+                curSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
