a840f02c8d03351f4d52f9689df455f1b8973b02
sapatevaibhav <sapatevaibhav@duck.com>
2024-03-10 21:46:11 +0530

    N

--- old/day1/nine.java
+++ old/day1/nine.java
@@ -1,16 +1,16 @@
-// Write a function that takes an array as an input and returns its length
+// // Write a function that takes an array as an input and returns its length
 
-public class nine {
-    public static void main(String[] args) {
-        System.out.println(arrLen(new int[] { 12, 2, 3, 444, 43 }));
-    }
+// public class nine {
+//     public static void main(String[] args) {
+//         System.out.println(arrLen(new int[] { 12, 2, 3, 444, 43 }));
+//     }
 
-    private static int arrLen(int[] is) {
-        int a = 0;
-        for (int i : is) {
-            a++;
-        }
-        return a;
-        // return is.length;
-    }
-}
+//     private static int arrLen(int[] is) {
+//         int a = 0;
+//         for (int i : is) {
+//             a++;
+//         }
+//         return a;
+//         // return is.length;
+//     }
+// }
--- old/day1/ten.java
+++ old/day1/ten.java
@@ -1,24 +1,24 @@
-// write a function that takes an object as input and returns number of properties it have
+// // write a function that takes an object as input and returns number of properties it have
 
-public class ten {
-    public static void main(String[] args) {
-        Innerten i = new Innerten("abc", 23, "kaij");
-        System.out.println(getCount(i));
-    }
+// public class ten {
+//     public static void main(String[] args) {
+//         Innerten i = new Innerten("abc", 23, "kaij");
+//         System.out.println(getCount(i));
+//     }
 
-    private static int getCount(Innerten i) {
-        return i.getClass().getDeclaredFields().length;
-    }
-}
+//     private static int getCount(Innerten i) {
+//         return i.getClass().getDeclaredFields().length;
+//     }
+// }
 
-class Innerten {
-    private String name;
-    private int age;
-    private String city;
+// class Innerten {
+//     private String name;
+//     private int age;
+//     private String city;
 
-    public Innerten(String name, int age, String city) {
-        this.name = name;
-        this.age = age;
-        this.city = city;
-    }
-}
+//     public Innerten(String name, int age, String city) {
+//         this.name = name;
+//         this.age = age;
+//         this.city = city;
+//     }
+// }
