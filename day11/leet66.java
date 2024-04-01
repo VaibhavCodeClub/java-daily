e6db7f305976f625a53a7cf9a03b68cabee24a3c
sapatevaibhav <sapatevaibhav@duck.com>
2024-04-01 19:46:11 +0530

    Plus One

---
+++ day11/leet66.java
@@ -0,0 +1,28 @@
+// Plus One
+// You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+import java.util.Arrays;
+
+public class leet66 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(plusOne(new int[] { 7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3,
+                7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 9, 9 })));
+        System.out.println(Arrays.toString(plusOne(new int[] { 9 })));
+        // plusOne(new int[] { 1, 2, 9 });
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        return result;
+    }
+}
