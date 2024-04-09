2ba03bffa1101ca2c9f54ed2ccfd29dddd960fd6
sapatevaibhav <sapatevaibhav@duck.com>
2024-04-09 21:36:36 +0530

    Time Needed to Buy Tickets

---
+++ day17/leet2073.java
@@ -0,0 +1,40 @@
+// Time Needed to Buy Tickets
+/*
+ * There are n people in a line queuing to buy tickets, where the 0th person is
+ * at the front of the line and the (n - 1)th person is at the back of the line.
+ * 
+ * You are given a 0-indexed integer array tickets of length n where the number
+ * of tickets that the ith person would like to buy is tickets[i].
+ * 
+ * Each person takes exactly 1 second to buy a ticket. A person can only buy 1
+ * ticket at a time and has to go back to the end of the line (which happens
+ * instantaneously) in order to buy more tickets. If a person does not have any
+ * tickets left to buy, the person will leave the line.
+ * 
+ * Return the time taken for the person at position k (0-indexed) to finish
+ * buying tickets.
+ */
+
+public class leet2073 {
+
+  public static void main(String[] args) {
+    System.out.println(timeRequiredToBuy(new int[] { 5, 1, 1, 1 }, 0));
+  }
+
+  public static int timeRequiredToBuy(int[] tickets, int k) {
+    int time = 0;
+    int i = 0;
+    while (tickets[k] != 0) {
+      if (i >= tickets.length) {
+        i = 0;
+      }
+      if (tickets[i] > 0) {
+        tickets[i] = tickets[i] - 1;
+        time++;
+        i++;
+      } else
+        i++;
+    }
+    return time;
+  }
+}
