7b41d511a65ce2551ca6a4a6c2e950b42afb117f
sapatevaibhav <sapatevaibhav@duck.com>
2024-06-03 21:05:17 +0530

    Largest Local Values in a Matrix

---
+++ day22/leet17.java
@@ -0,0 +1,19 @@
+// Largest Local Values in a Matrix
+// Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+// import java.util.HashMap;
+import java.util.List;
+// import java.util.Map;
+
+public class leet17 {
+  public static void main(String[] args) {
+    System.out.println(letterCombinations("23"));
+  }
+
+  public static List<String> letterCombinations(String digits) {
+    // Map solMap<> = new HashMap<>();
+    return null;
+  }
+}
