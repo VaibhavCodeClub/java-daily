20d400894b756f6dfb6645cd68bc74aaa6d80f20
sapatevaibhav <sapatevaibhav@duck.com>
2024-09-19 20:39:53 +0530

    Missing Number

--- day13 sort/leet268.java
+++ day13 sort/leet268.java
@@ -5,27 +5,30 @@
 // Used in amazon
 
 public class leet268 {
-    public static void main(String[] args) {
-        System.out.println(missingNumber(new int[] { 0, 1 }));
-    }
 
-    public static int missingNumber(int[] nums) {
-        for (int i = 0; i < nums.length;) {
-            if (nums[i] == i || nums[i] == nums.length) {
-                // If number is at correct position or is equal to the length of array ignore it
-                i++;
-                continue;
-            }
-            // Store all the elements at it's correct position using swapping
-            int temp = nums[nums[i]];
-            nums[nums[i]] = nums[i];
-            nums[i] = temp;
-        }
-        for (int i = 0; i < nums.length; i++)
-            // Iterate through the entir earray again and when index mismatches the value
-            // return the value
-            if (nums[i] != i)
-                return i;
-        return nums.length;
+  public static void main(String[] args) {
+    System.out.println(missingNumber(new int[] { 0, 2 }));
+  }
+
+  public static int missingNumber(int[] nums) {
+    for (int i = 0; i < nums.length;) {
+      if (nums[i] == i || nums[i] == nums.length) {
+        // If number is at correct position or is equal to the length of array ignore it
+        i++;
+        continue;
+      }
+      // Store all the elements at it's correct position using swapping
+      int temp = nums[nums[i]];
+      nums[nums[i]] = nums[i];
+      nums[i] = temp;
     }
+    for (
+      int i = 0;
+      i < nums.length;
+      i++
+    ) // Iterate through the entire array again and when index mismatches the value
+    // return the value
+    if (nums[i] != i) return i;
+    return nums.length;
+  }
 }
