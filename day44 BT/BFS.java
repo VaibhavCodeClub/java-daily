bfac38b1a080b9f101613ac87a76c99d46075cba
sapatevaibhav <sapatevaibhav@duck.com>
2024-11-01 13:35:44 +0530

    breadth first search

---
+++ day44 BT/BFS.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+public class BFS {
+
+  private static class Node {
+
+    int val;
+    Node left;
+    Node right;
+
+    public Node(int data) {
+      this.val = data;
+    }
+  }
+
+  public static void main(String[] args) {
+    Node root = new Node(1);
+    root.left = new Node(4);
+    root.right = new Node(3);
+    root.left.left = new Node(2);
+    root.left.right = new Node(9);
+    root.right.left = new Node(1);
+    root.right.right = new Node(7);
+
+    // System.out.println(bfs(root));
+    System.out.println(levelOrder(root));
+  }
+
+  private static List<List<Integer>> bfs(Node root) {
+    List<List<Integer>> ans = new LinkedList<>();
+    if (root == null) return ans;
+
+    Queue<Node> queue = new LinkedList<>();
+    queue.offer(root);
+
+    while (!queue.isEmpty()) {
+      int size = queue.size();
+      List<Integer> sublList = new ArrayList<>();
+
+      for (int i = 0; i < size; i++) {
+        if (queue.peek().left != null) queue.offer(queue.peek().left);
+        if (queue.peek().right != null) queue.offer(queue.peek().right);
+        sublList.add(queue.poll().val);
+      }
+      ans.add(sublList);
+    }
+    return ans;
+  }
+
+  private static List<Integer> levelOrder(Node root) {
+    List<Integer> ans = new ArrayList<>();
+    if (root == null) return ans;
+
+    Queue<Node> queue = new LinkedList<>();
+    queue.add(root);
+
+    while (!queue.isEmpty()) {
+      Node curr = queue.poll();
+
+      ans.add(curr.val);
+
+      if (curr.left != null) queue.add(curr.left);
+      if (curr.right != null) queue.add(curr.right);
+    }
+    return ans;
+  }
+}
